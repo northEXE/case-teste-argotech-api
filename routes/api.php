e4967c9635acb09db70f2f19f7cc652b084881a7
northEXE <wendel.ferreira.wfm@hotmail.com>
2023-10-17 09:52:25 -0300

    Acrescentando o endpoint de marcar tarefa como feito

--- routes/api.php
+++ routes/api.php
@@ -6,6 +6,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/to-do-list', [TaskController::class, 'index']);
 Route::post('/to-do-list', [TaskController::class, 'store']);
 Route::put('/to-do-list/{id}', [TaskController::class, 'update']);
+Route::put('/to-do-list/{id}/markAsDone', [TaskController::class, 'markAsDone']);
 Route::put('/to-do-list/{id}/markAsUndone', [TaskController::class, 'markAsUndone']);
 Route::delete('/to-do-list/{id}', [TaskController::class, 'delete']);
 
